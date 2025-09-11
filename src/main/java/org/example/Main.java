1e19088a75fc83ddd187e3e9add3b1915a5f1121
anaquijano1 <anamargaritaq@gmail.com>
2025-09-11 14:06:29 -0400

    Attempt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,41 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        String fileName = null;
+        int numberR=435;
+        boolean methodHamilton=false;
+
+        int numberOfArguments= args.length;
+        System.out.println("Number of arguments: " + numberOfArguments);
+        switch (numberOfArguments) {
+            case 1:
+                fileName=args[0];
+                break;
+            case 2:
+                fileName = args[0];
+                if (args[1].charAt(2)=='h'){
+                    methodHamilton=true;
+                }else{
+                    numberR=Integer.parseInt(args[1]);
+                }
+                break;
+            case 3:
+                fileName = args[0];
+                numberR=Integer.parseInt(args[1]);
+                if (args[2].charAt(2)=='h'){
+                    methodHamilton=true;
+                }
+
+        }
+        System.out.println("Number of arguments: "+numberOfArguments);
+        System.out.println("Method: "+methodHamilton);
+        System.out.println("Number of rep: "+numberR);
+        System.out.println("File name: "+fileName);
+
+
+
+
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -20,7 +54,7 @@ public class Main {
         System.out.println(car2);
         System.out.println("HELLO");
         System.out.println("This is attempt");
-        System.out.println("CHANGE");
+        System.out.println("commit");
 
 
 
